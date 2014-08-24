b04ce0e75a1c87363759e54792d04ab665b8905c
Nathan Skillen <nskillen@gmail.com>
2014-08-24 10:56:09 -0700

    switched to using code::blocks. Maybe this time I'll stick with it :) removed Makefile removed object file that should never have been checked in. code now passes unit tests in separate tests project

--- LBXFile.cpp
+++ LBXFile.cpp
@@ -6,70 +6,62 @@
 #include <new>
 #include <stdexcept>
 
-using namespace OpenMOO2::LBX;
-
 std::vector<char> get_file_contents(std::string);
 
-/**
- * Reads an LBX file from disk, and returns a de-serialized LBXFile object
- * Params:
- * filename - the name of the file to read
- **/
-LBXFile LBXFile::read_file(std::string filename) {
-    LBXFile lbx;
-    lbx.filename = filename;
-    lbx.deserialize(get_file_contents(filename));
-    return lbx;
+using namespace OpenMOO2::LBX;
+
+LBXFile::LBXFile()
+: _filename(""), _version(LBX_DEFAULT_VERSION), junk_data(0), files(0)
+{
+	junk_data.resize(0);
+	files.resize(0);
 }
 
-
-/**
- * Creates a new LBX file.
- * Params:
- * filename - the name of the LBX file to create
- * files_to_pack - the names of the files to be packed into the LBX file
- * version - the version of the LBX file, default is 0
- **/
-void LBXFile::write_file(std::string filename, LBXFile & lbx) {
-    write_file(filename, &lbx);
+LBXFile::LBXFile(const LBXFile & other)
+: _filename(other._filename), _version(other._version), junk_data(other.junk_data), files(other.files)
+{
+	junk_data.resize(other.junk_data.size());
+	files.resize(other.files.size());
 }
 
-void LBXFile::write_file(std::string filename, LBXFile * lbx) {
-    std::ofstream file(filename, std::ios::trunc|std::ios::binary);
+LBXFile::LBXFile(LBXFile && other)
+: _filename(other._filename), _version(other._version), junk_data(other.junk_data), files(other.files)
+{
+	junk_data.resize(other.junk_data.size());
+	files.resize(other.files.size());
+}
 
-    if (!file.is_open()) {
-        throw std::runtime_error("Unable to open " + filename + " for writing");
+LBXFile::LBXFile(std::string filename)
+: _filename(filename), _version(LBX_DEFAULT_VERSION), junk_data(0), files(0)
+{
+	junk_data.resize(0);
+	files.resize(0);
+    deserialize(filename);
+}
+
+/**
+ * Serializes the LBX file object into a file on disk
+ **/
+void LBXFile::serialize() const {
+    if (0 == _filename.compare("")) {
+        throw std::runtime_error("Cannot serialize without a filename");
     }
 
-    std::vector<char> lbx_data = lbx->serialize();
-    file.write(lbx_data.data(), lbx_data.size());
-
-    if (file.fail()) {
-        file.close();
-        throw std::runtime_error("Error while writing to file: " + filename);
+    if (0 == files.size()) {
+        throw std::runtime_error("Must pack at least one file to serialize LBX");
     }
 
-    file.flush();
-    file.close();
-}
-
-/**
- * Serializes the LBX file object into a bytestream
- **/
-const std::vector<char> LBXFile::serialize() const {
     std::vector<char> lbx_data;
     size_t file_size = 0;
+    uint32_t offset;
 
     file_size += LBX_HEADER_SIZE; // fixed portion of header
-
-    file_size += 4 * (files.size()+1); // +4 bytes per file, with one extra
-
-    for (LBXPackedFile* packed_file : files) { // plus all the file sizes
-        file_size += packed_file->size();
+    file_size += LBX_OFFSET_SIZE * (files.size()+1); // +4 bytes per file, with one extra
+    file_size += junk_data.size();
+    offset = file_size; // initial file offset is just past the end of the header
+    for (const std::vector<char> & packed_file : files) { // plus all the file sizes
+        file_size += packed_file.size();
     }
-
-    file_size = std::max(static_cast<uint32_t>(file_size), files[files.size()-1]->ending_offset());
-
     lbx_data.resize(file_size); // set vector to appropriate size
 
     // first two bytes are number of files
@@ -77,63 +69,94 @@ const std::vector<char> LBXFile::serialize() const {
     // next four bytes are LBX signature 0xFEAD
     *reinterpret_cast<uint32_t*>(lbx_data.data()+2) = VERIFICATION_CODE_VALUE;
     // next two bytes are the version number
-    *reinterpret_cast<uint16_t*>(lbx_data.data()+6) = version;
+    *reinterpret_cast<uint16_t*>(lbx_data.data()+6) = _version;
+
+    std::copy(junk_data.begin(), junk_data.end(), lbx_data.begin()+(offset-junk_data.size()));
 
     size_t file_num = 0;
     for (auto file : files) {
         // now four bytes indicate the start offset for each file in the LBX
-        *reinterpret_cast<uint32_t*>(lbx_data.data() + 8 + (4 * file_num)) = file->starting_offset();
+        *reinterpret_cast<uint32_t*>(lbx_data.data() + LBX_HEADER_SIZE + (LBX_OFFSET_SIZE * file_num)) = offset;
         file_num += 1;
 
         // and while we're here, let's copy the packed file data over
-        std::copy(file->data().begin(), file->data().end(), lbx_data.begin() + file->starting_offset());
+        std::copy(file.begin(), file.end(), lbx_data.begin() + offset);
+        offset += file.size();
+    }
+    // and 4 bytes for the end of the last file
+    *reinterpret_cast<uint32_t*>(lbx_data.data() + LBX_HEADER_SIZE + (LBX_OFFSET_SIZE * file_num)) = offset;
+
+    // everything is copied now, let's write it to disk
+    std::ofstream file(_filename, std::ios::binary|std::ios::trunc);
+    if (!file.is_open()) {
+        throw std::runtime_error("Unable to open file "+_filename+" for writing");
     }
 
-    // and 4 bytes for the end of the last file
-    *reinterpret_cast<uint32_t*>(lbx_data.data() + 8 + (4 * file_num)) = file_size;
-
-    // everything is copied now, let's give it back
-    return lbx_data;
+    file.write(lbx_data.data(), lbx_data.size());
+    file.flush();
+    file.close();
 }
 
 /**
- * De-serializes the LBX file from a bytestream into the object
+ * De-serializes the LBX file from a file into the object
  * Params:
- * lbx_data - the bytestream to deserialize
+ * filename - the LBX file to deserialize
  **/
-void LBXFile::deserialize(std::vector<char> lbx_data) {
-    version = lbx_data[6] + static_cast<uint16_t>(256 * lbx_data[7]);
+void LBXFile::deserialize(std::string filename) {
+    auto data = get_file_contents(filename); // read file
 
-    uint32_t data_sig = *reinterpret_cast<uint32_t*>(lbx_data.data()+2);
-
+	// validate file signature
+    uint32_t data_sig = *reinterpret_cast<uint32_t*>(data.data()+2);
     if (VERIFICATION_CODE_VALUE != data_sig) {
         throw std::runtime_error("Provided data signature is incorrect");
     }
 
-    size_t num_files = *reinterpret_cast<uint16_t*>(lbx_data.data());
+	// get file version
+    _version = data[6] + static_cast<uint16_t>(256 * data[7]);
 
-    files.resize(num_files, nullptr);
+	// get number of files
+    size_t num_files = *reinterpret_cast<uint16_t*>(data.data());
+    files.resize(num_files);
+
+    uint32_t hdr_end = LBX_HEADER_SIZE + (LBX_OFFSET_SIZE * (files.size()+1));
+    uint32_t files_begin = *reinterpret_cast<uint32_t*>(data.data()+LBX_HEADER_SIZE);
+    junk_data.resize(files_begin - hdr_end);
+    std::copy(data.begin()+hdr_end, data.begin()+files_begin, junk_data.begin());
 
     for (size_t i = 0; i < num_files; ++i) {
-        files[i] = new LBXPackedFile();
-        files[i]->starting_offset(*reinterpret_cast<uint32_t*>(lbx_data.data()+8+(4*i)));
-        files[i]->ending_offset(*reinterpret_cast<uint32_t*>(lbx_data.data()+8+(4*(i+1))));
-        std::copy(lbx_data.begin()+files[i]->starting_offset(), lbx_data.begin()+files[i]->ending_offset(), files[i]->data().begin());
+        uint32_t start = *reinterpret_cast<uint32_t*>(data.data()+LBX_HEADER_SIZE+(LBX_OFFSET_SIZE*i));
+        uint32_t end = *reinterpret_cast<uint32_t*>(data.data()+LBX_HEADER_SIZE+(LBX_OFFSET_SIZE*(i+1)));
+        files[i].resize(end-start);
+        std::copy(data.begin()+start, data.begin()+end, files[i].begin());
     }
+    _filename = filename;
 }
 
-std::string LBXFile::get_filename() const {
-    return filename;
+std::string LBXFile::filename() const {
+    return _filename;
+}
+
+void LBXFile::filename(std::string filename) {
+	_filename = filename;
+}
+
+uint16_t LBXFile::version() const {
+	return _version;
+}
+
+void LBXFile::version(uint16_t version) {
+	_version = version;
 }
 
 size_t LBXFile::size() const {
+    if (0 == _filename.compare("")) { return 0; }
     size_t file_size = 0;
-    file_size += 8; // fixed header size
-    file_size += 4 * files.size(); // 4 bytes for each file start offset
+    file_size += LBX_HEADER_SIZE; // fixed header size
+    file_size += LBX_OFFSET_SIZE * (files.size()+1); // 4 bytes for each file start offset, plus 4 bytes for final offset
+    file_size += junk_data.size(); // file may contain junk after offsets, but before file data
     if (files.size() > 0) {
-        file_size += 1; // plus 1 because each file has an end offset (that is the start offset of the next file)
         for (auto file : files) {
-            file_size += file->size(); // plus the size of the individual files
+            file_size += file.size(); // plus the size of the individual files
         }
     }
     return file_size;
@@ -143,7 +166,11 @@ size_t LBXFile::num_packed_files() const {
     return files.size();
 }
 
-LBXPackedFile* LBXFile::get_file_number(size_t file_num) const {
+std::vector<char> LBXFile::get_file_number(size_t file_num) const {
+    if (files.size() <= file_num) {
+        throw std::runtime_error("Invalid file number specified");
+    }
+
     return files[file_num];
 }
 
@@ -152,38 +179,24 @@ void LBXFile::pack_file(std::string filename) {
 }
 
 void LBXFile::pack_file(std::string filename, size_t file_num) {
-    std::vector<char> file_data = get_file_contents(filename);
-
-    auto files_iter = files.begin() + file_num - (file_num > 0 ? 1 : 0);
-
-    LBXPackedFile* packed_file = new LBXPackedFile();
-    packed_file->starting_offset(file_num > 0 ? (*files_iter)->ending_offset() : 0);
-    packed_file->ending_offset(packed_file->starting_offset() + file_data.size());
-    packed_file->data(file_data);
-
-    uint32_t new_offset = packed_file->ending_offset();
-
-    files.insert(files.begin() + file_num, packed_file);
-    for (auto iter = files.begin() + file_num + 1; iter != files.end(); ++iter) {
-        (*iter)->starting_offset(new_offset);
-        new_offset += (*iter)->size();
-        (*iter)->ending_offset(new_offset);
-    }
+    files.insert(files.begin() + file_num, get_file_contents(filename));
 }
 
 void LBXFile::remove_file(size_t file_num) {
+    if (files.size() <= file_num) {
+        throw std::runtime_error("Invalid file number specified for removal");
+    }
     files.erase(files.begin() + file_num);
+}
 
-    auto new_offset = 0;
-    if (file_num > 0) {
-        new_offset = files[file_num-1]->ending_offset();
-    }
-
-    for (auto iter = files.begin() + file_num + 1; iter != files.end(); ++iter) {
-        (*iter)->starting_offset(new_offset);
-        new_offset += (*iter)->size();
-        (*iter)->ending_offset(new_offset);
-    }
+LBXFile & LBXFile::operator=(const LBXFile & other) {
+    _filename = other._filename;
+    _version = other._version;
+    junk_data.resize(other.junk_data.size());
+    std::copy(other.junk_data.begin(), other.junk_data.end(), junk_data.begin());
+    files.resize(other.files.size());
+    std::copy(other.files.begin(), other.files.end(), files.begin());
+    return *this;
 }
 
 std::vector<char> get_file_contents(std::string filename) {
--- LBXFile.h
+++ LBXFile.h
@@ -5,8 +5,6 @@
 #include <string>
 #include <vector>
 
-#include "LBXPackedFile.h"
-
 /**
  * This file contains the necessary information to decode a MOO2 LBX File.
  * The details of the LBX file structure were taken from:
@@ -15,35 +13,95 @@
  * https://web.archive.org/web/20050310012845/http://www.xprt.net/~s8/prog/orion2/lbx/
  **/
 
+/**
+ * LBX File header structure is as follows:
+ * (each line is 1b)
+ * ----------------
+ * | Number of files (n)
+ * |
+ * ----------------
+ * | Verification Code
+ * |
+ * |
+ * |
+ * ----------------
+ * | Version
+ * |
+ * ----------------
+ * | Offset 1
+ * |
+ * |
+ * |
+ * ----------------
+ * | Offset 2
+ * |
+ * |
+ * |
+ * ----------------
+ * | ...
+ * ----------------
+ * | Offset n
+ * |
+ * |
+ * |
+ * ----------------
+ * | Offset n+1
+ * | aka
+ * | End of last file
+ * |
+ * ----------------
+ * | possible junk space (arbitrary size, may not be present)
+ * ----------------
+ * | file data
+ * | .
+ * | .
+ * | .
+ * ----------------
+ * (eof)
+ **/
+
 namespace OpenMOO2 {
 namespace LBX {
 
-const uint16_t LBX_DEFAULT_VERSION = 0x00;
+const uint16_t LBX_DEFAULT_VERSION     = 0x00;
 const uint32_t VERIFICATION_CODE_VALUE = 0xFEAD;
-const uint32_t EOF_MARK = 0x03E8;
-const size_t LBX_HEADER_SIZE = 0x08;
+const uint32_t EOF_MARK                = 0x03E8;
+const size_t   LBX_HEADER_SIZE         = 0x08;
+const size_t   LBX_OFFSET_SIZE         = 0x04;
 
 class LBXFile {
 public:
-    static LBXFile read_file(std::string);
-    static void write_file(std::string, LBXFile&);
-    static void write_file(std::string, LBXFile*);
+    LBXFile();
+    LBXFile(const LBXFile&);
+    LBXFile(LBXFile&&);
+    LBXFile(std::string);
 
-    const std::vector<char> serialize() const;
-    void deserialize(std::vector<char>);
+    void serialize() const;
+    void deserialize(std::string);
 
-    std::string get_filename() const;
+    std::string filename() const;
+    void filename(std::string filename);
+
+    uint16_t version() const;
+    void version(uint16_t);
+
     size_t size() const;
+
     size_t num_packed_files() const;
-    LBXPackedFile* get_file_number(size_t) const;
+    std::vector<char> get_file_number(size_t) const;
+
     void pack_file(std::string);
     void pack_file(std::string, size_t);
+
     void remove_file(size_t);
 
+    LBXFile & operator=(const LBXFile&);
+
 private:
-    std::string filename;
-    uint16_t version;
-    std::vector<LBXPackedFile*> files;
+    std::string _filename;
+    uint16_t _version;
+    std::vector<char> junk_data;
+    std::vector<std::vector<char>> files;
 };
 
 }}
